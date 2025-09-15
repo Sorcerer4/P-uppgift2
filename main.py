03ccb99ace5bdf84444abf1f237b3380a39a6c46
Arvid <hultarvid@gmail.com>
2025-09-15 20:49:10 +0200

    changes main code to make the sphere an object which calculates its own brightness based on cond

--- main.py
+++ main.py
@@ -1,50 +1,86 @@
 from math import *
 from sys import *
 
-
-class pixel:
-    def __init__(self,r,x,y,x0,y0,z0,token_list):
-        self.x = x
-        self.y = y
-        self.b = self.calc_b(r,x0,y0,z0)
+class sphere:
+    def __init__(self,r,x0,y0,token_list, res):
+        self.x0 = x0
+        self.y0 = y0
+        self.z0 = self.calc_z0(r, x0,y0)
         self.token_list = token_list
-        self.token = self.token_translate()
+        self.brightness = self.rayTrace(r,res)
+        self.tokens = self.token_translate(self.brightness)
 
-    def calc_b(self, r, x0,y0,z0):
-        disc = r**2-self.x**2-self.y**2
+    def rayTrace(self, r, res):
+
+        x_res, y_res = res
+        b_matrix = []
+
+        for y_pixel in range(y_res + 1):
+            y = -r + y_pixel * (2*r / float(y_res))
+            row = []
+
+            for x_pixel in range(x_res + 1):
+                x = -r + x_pixel * (2*r / float(x_res))
+
+                #Main Brightness calculation
+                b = self.calc_b(r,x,y)
+
+                row.append(b)
+            b_matrix.append(row)
+
+        return b_matrix
+
+    def calc_z0(self,r,x0,y0):
+
+        try:
+            z0 = sqrt(r ** 2 - x0 ** 2 - y0 ** 2)
+        except ValueError:
+            print("Error:Light origin outside of target sphere")
+            exit()
+        return z0
+
+    def calc_b(self, r, x, y):
+        disc = r**2-x**2-y**2
 
         if disc < 0:
             b = 0
         else:
             z = sqrt(disc)
-            b = (self.x*x0+self.y*y0+z*z0)/r**2
+            b = (x*self.x0+y*self.y0+z*self.z0)/r**2
 
         return b
-    def token_translate(self):
-        if self.b <= 0:
-            token = self.token_list[0]
-        elif 0 < self.b <= 0.3:
-            token = self.token_list[1]
-        elif 0.3 < self.b <= 0.5:
-            token = self.token_list[2]
-        elif 0.5 < self.b <= 0.7:
-            token = self.token_list[3]
-        elif 0.7 < self.b <= 0.9:
-            token = self.token_list[4]
-        elif 0.9 < self.b <= 1:
-            token = self.token_list[5]
-        else:
-            print("Error no matching token found")
-            exit()
 
-        return token
+    def token_translate(self, b_vals):
+        print(numpy.shape(b_vals))
+        count = 0
+        t_matrix = []
+        for b_row in b_vals:
+            t_row = []
+            for b in b_row:
+                if b <= 0:
+                    t_row.append(self.token_list[0])
+                elif 0 < b <= 0.3:
+                    t_row.append(self.token_list[1])
+                elif 0.3 < b <= 0.5:
+                    t_row.append(self.token_list[2])
+                elif 0.5 < b <= 0.7:
+                    t_row.append(self.token_list[3])
+                elif 0.7 < b <= 0.9:
+                    t_row.append(self.token_list[4])
+                elif 0.9 < b <= 1:
+                    t_row.append(self.token_list[5])
+                else:
+                    print("Error no matching token found")
+                    exit()
+
+            t_matrix.append(t_row)
+        return t_matrix
 
 
 
 def read():
     #Params:
-    x_resolution = 100
-    y_resolution = 100
+    res =  [60, 60]
     """"#TESTING
     r = float(input("Input sphere radius: "))
     x0 = float(input("Input light origin x cord: "))
@@ -52,45 +88,36 @@ def read():
     """
 
     #TESTING
-    r = 5
-    x0 = 3
-    y0 = 2
+    r = 4
+    x0 = 1
+    y0 = 0
 
     token_list = ["M", "*", "+", "-","."," "]
 
-    return r,x0,y0, x_resolution,y_resolution,token_list
+    return r,x0,y0,token_list, res
 
 
-def calc_light_vals(r, x0, y0,x_res, y_res,token_list):
 
-    #Calculate z0
-    try: z0 = sqrt(r**2-x0**2-y0**2)
-    except ValueError:
-        print("Error:Light origin outside of target sphere")
-        exit()
 
-    pixel_list = []
-    for step in range(y_res + 1):
-        y = -r + step*(2 * r / float(y_res))
-        for step in range(x_res + 1):
-            x = -r + step * (2 * r / float(x_res))
 
-            pixel_list.append(pixel(r,x,y,x0,y0,z0,token_list))
-
-    return pixel_list
+    return
 
             #Calculation for given (x,y) pa
-
 def main():
 
     #Read radius and light origin (x and y)
-    r, x0, y0, x_resolution, y_resolution, token_list = read()
-    pixel_list = calc_light_vals(r, x0, y0, x_resolution, y_resolution, token_list)
+    r, x0, y0, token_list, res= read()
+    s = sphere(r,x0,y0,token_list,res)
 
+    for y in s.tokens:
+        print(y)
+
+
+"""
     output_string = ""
     count = 1
     for pixel in pixel_list:
-        output_string = output_string + "" + pixel.token
+        output_string = output_string + " " + pixel.token
         if count%(x_resolution+1) == 0:
             output_string += ","''
         count+=1
@@ -98,10 +125,7 @@ def main():
     outputlist = output_string.split(",")
     for x in outputlist:
         print(x)
-
-    return
-
-
+"""
 
 
 if __name__ == '__main__':
