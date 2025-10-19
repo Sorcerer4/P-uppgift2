b78973e2f94dd7a2865a106f4bd5cca2b948205c
Arvid <hultarvid@gmail.com>
2025-10-19 22:31:35 +0200

    Calc shadow cord, store in sphere, amount of b vals only dependent on r and not res. Res to be used for scenery graphics

--- main.py
+++ main.py
@@ -2,47 +2,69 @@ from math import *
 from sys import *
 import pygame
 import xlwings as xw
+
+"""class Scenery:
+    def __init__(self):
+        
+"""
 class Sphere:
-    def __init__(self,r,x0,y0,token_list, res):
+    def __init__(self,r,x0,y0,token_list):
         self.x0 = x0
         self.y0 = y0
-        self.z0 = self.calc_z0(r, x0,y0)
+        self.z0 = self.calc_z(r, x0, y0)
         self.token_list = token_list
-        self.brightness, self.tokens = self.ray_trace(r, res)
+        self.brightness, self.tokens, self.shadow_cords = self.ray_trace(r)
 
-    def ray_trace(self, r, res):
+    def ray_trace(self, r):
 
-        x_res, y_res = res
         b_matrix = []
         t_matrix = []
+        shadow_cords = []
 
-        for y_pixel in range(y_res + 1):
-            y = -r + y_pixel * (2*r / float(y_res))
+        for ry_Step in range (2*r):
+            y = -r + ry_Step
+
             b_row = []
             t_row = []
 
-            for x_pixel in range(x_res + 1):
-                x = -r + x_pixel * (2*r / float(x_res))
+            for rx_Step in range(2*r):
+                x = -r + rx_Step
 
                 #Main Brightness calculation
                 b = self.calc_b(r,x,y)
-
                 b_row.append(b)
+
+                #Translate Brightness to token
                 t_row.append(self.token_translate(b))
+
+                #Calculate Shadow
+                shadow = self.shadow_calc(r,x,y)
+                if shadow is not None: shadow_cords.append(shadow)
 
             b_matrix.append(b_row)
             t_matrix.append(t_row)
 
-        return (b_matrix, t_matrix)
+        return (b_matrix, t_matrix, shadow_cords)
 
-    def calc_z0(self,r,x0,y0):
+    def shadow_calc(self, r, x,y):
+        disc = r**2-x**2-y**2
+        x0 = self.x0
+        y0 = self.y0
+        z0 = self.z0
+        if disc > 0:
+            shadow_x = x0 - (z0 * (x-x0)) / 2
+            shadow_y = y0 - (z0 * (y - y0)) / 2
+            return [shadow_x, shadow_y]
+
+
+    def calc_z(self,r,x0,y0):
 
         try:
-            z0 = sqrt(r ** 2 - x0 ** 2 - y0 ** 2)
+            z = sqrt(r ** 2 - x0 ** 2 - y0 ** 2)
         except ValueError:
             print("Error:Light origin outside of target sphere")
             exit()
-        return z0
+        return z
 
     def calc_b(self, r, x, y):
         disc = r**2-x**2-y**2
@@ -82,6 +104,11 @@ class Sphere:
             print("Error no matching token found")
             exit()
 
+
+class GUI():
+    def __init__(self):
+        self.surface = pygame.Surface()
+
 def read():
     debugmode = True
 
@@ -113,11 +140,17 @@ def read():
             if debugmode: print("pass pos_disc")
             break
         else:
-            x0, y0 = input("Negativ discriminant, light source outside of range, please enter new values manually (X,Y): ")
+            x0, y0 = input("Negativ discriminant, light source outside of range, please enter new values manually (X,Y): ").split()
+            x0, y0 = int(x0), int(y0)
+
 
 
 
     return r,x0,y0,token_list, res
+def shadow():
+
+
+    return
 
 def main():
 
@@ -125,13 +158,14 @@ def main():
     r, x0, y0, token_list, res= read()
 
     #Calculate sphere
-    s = Sphere(r,x0,y0,token_list,res)
+    s = Sphere(r,x0,y0,token_list)
 
     for row in s.tokens:
         printline=""
         for token in row:
             printline += token
         print(printline)
+
 
 
 
